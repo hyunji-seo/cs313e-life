755fb3a7397aff8ed6c225b98cf605b096ecc84f
hyunji-seo <hjseo767@gmail.com>
2014-11-25 15:45:04 -0600

    fix count

--- TestLife.py
+++ TestLife.py
@@ -3,6 +3,24 @@ from unittest import main, TestCase
 from Life import Life, AbstractCell, ConwayCell, FredkinCell, life_read
 
 class TestLife(TestCase):
+	# AC init
+	def test_ACinit_1(self):
+		cell = ConwayCell('*', 1, 5)
+		self.assertEqual(cell.state, '*')
+		self.assertEqual(cell.x, 1)
+		self.assertEqual(cell.y, 5)
+
+	def test_ACinit_2(self):
+		cell = ConwayCell('.', 2, 3)
+		self.assertEqual(cell.state, '.')
+		self.assertEqual(cell.x, 2)
+		self.assertEqual(cell.y, 3)
+
+	def test_ACinit_3(self):
+		cell = FredkinCell('-', 3, 9)
+		self.assertEqual(cell.state, '-')
+		self.assertEqual(cell.x, 3)
+		self.assertEqual(cell.y, 9)
 
 	# change state
 	def test_change_state_1(self):
@@ -16,16 +34,21 @@ class TestLife(TestCase):
 		self.assertEqual(cell.future, '*')
 
 	def test_change_state_3(self):
+		cell = ConwayCell('.', 6, 5)
+		cell.change_state()
+		self.assertEqual(cell.future, '*')		
+
+	def test_change_state_4(self):
 		cell = FredkinCell('-', 3, 9)
 		cell.change_state()
 		self.assertEqual(cell.future, 0)
 
-	def test_change_state_4(self):
+	def test_change_state_5(self):
 		cell = FredkinCell('0', 5, 9)
 		cell.change_state()
 		self.assertEqual(cell.future, '-')
 
-	def test_change_state_5(self):
+	def test_change_state_6(self):
 		cell = FredkinCell('-', 6, 4)
 		cell.change_state()
 		self.assertEqual(cell.future, 0)
@@ -55,14 +78,91 @@ class TestLife(TestCase):
 		cell.state = '*'
 		cell.count = 0
 
-	# print grid
+	# CC init
+	def test_CCinit_1(self):
+		cell = ConwayCell('.', 2, 3)
+		self.assertEqual(cell.state, '.')
+		self.assertEqual(cell.x, 2)
+		self.assertEqual(cell.y, 3)
+		self.assertEqual(cell.type_cell, 'Conway')
+
+	def test_CCinit_2(self):
+		cell = ConwayCell('*', 5, 3)
+		self.assertEqual(cell.state, '*')
+		self.assertEqual(cell.x, 5)
+		self.assertEqual(cell.y, 3)
+		self.assertEqual(cell.type_cell, 'Conway')
+
+	def test_CCinit_3(self):
+		cell = ConwayCell('.', 9, 3)
+		self.assertEqual(cell.state, '.')
+		self.assertEqual(cell.x, 9)
+		self.assertEqual(cell.y, 3)
+		self.assertEqual(cell.type_cell, 'Conway')
+
+	# FC init
+	def test_FCinit_1(self):
+		cell = FredkinCell('-', 9, 3)
+		self.assertEqual(cell.state, '-')
+		self.assertEqual(cell.x, 9)
+		self.assertEqual(cell.y, 3)
+		self.assertEqual(cell.type_cell, 'Fredkin')
+
+	def test_FCinit_2(self):
+		cell = FredkinCell('0', 4, 5)
+		self.assertEqual(cell.state, 0)
+		self.assertEqual(cell.age, 0)
+		self.assertEqual(cell.x, 4)
+		self.assertEqual(cell.y, 5)
+		self.assertEqual(cell.type_cell, 'Fredkin')
+
+	def test_FCinit_3(self):
+		cell = FredkinCell('-', 3, 4)
+		self.assertEqual(cell.state, '-')
+		self.assertEqual(cell.x, 3)
+		self.assertEqual(cell.y, 4)
+		self.assertEqual(cell.type_cell, 'Fredkin')
+
+	# L init
+	def test_Linit_1(self):
+		x = Life(2, 15, [['.','.','*','.','.','.','.','.','*','.','.','.','.','.','.'], ['.','.','.','.','.','.','.','.','.','*','.','.','.','.','.']])
+		self.assertEqual(x.row, 2)
+		self.assertEqual(x.col, 15)
+		self.assertEqual(x.population, 3)
+		self.assertEqual(x.generation, 0)
+
+	def test_Linit_2(self):
+		x = Life(2, 2, [['.','.'], ['*','*']])
+		self.assertEqual(x.row, 2)
+		self.assertEqual(x.col, 2)
+		self.assertEqual(x.population, 2)
+		self.assertEqual(x.generation, 0)
+
+	def test_Linit_3(self):
+		x = Life(5, 3, [['.','.','.'], ['*','.','.'], ['*','*','*'], ['.','.','*'], ['.','.','*']])
+		self.assertEqual(x.row, 5)
+		self.assertEqual(x.col, 3)
+		self.assertEqual(x.population, 6)
+		self.assertEqual(x.generation, 0)
+
+	# simulate
 	def test_simulate_1(self):
 		x = Life(2, 15, [['.','.','*','.','.','.','.','.','*','.','.','.','.','.','.'], ['.','.','.','.','.','.','.','.','.','*','.','.','.','.','.']])
 		x.moat_grid()
 		x.simulate()
 		x.generation = 1
 
+	def test_simulate_2(self):
+		x = Life(2, 15, [['.','.','*','.','.','.','.','.','*','.','.','.','.','.','.'], ['.','.','.','.','.','.','.','.','.','*','.','.','.','.','.']])
+		x.moat_grid()
+		x.simulate()
+		x.generation = 1
 
+	def test_simulate_3(self):
+		x = Life(2, 2, [['.','.'], ['*','*']])
+		x.moat_grid()
+		x.simulate()
+		x.generation = 1
 
 	# count pop
 	def test_count_pop_1(self):
@@ -186,9 +286,46 @@ class TestLife(TestCase):
 		col = 4
 		self.assertEqual(True, x.is_self(row, col, 2, 4))	
 
+	# count (row + 1)
+	def test_count_1(self):
+		x = Life(5, 3, [['-','-','-'], ['0','1','0'], ['0','-','-'], ['-','-','0'], ['*','0','-']])
+		x.moat_grid()
+		x.count()
+		self.assertEqual(x.grid[2][2].count, 2)
+	
+	def test_count_2(self):
+		x = Life(5, 3, [['-','-','-'], ['0','1','0'], ['0','-','-'], ['-','-','0'], ['*','0','-']])
+		x.moat_grid()
+		x.count()
+		self.assertEqual(x.grid[2][1].count, 2)
+	
+	def test_count_3(self):
+		x = Life(5, 3, [['-','-','-'], ['0','1','0'], ['0','-','-'], ['-','-','0'], ['*','0','-']])
+		x.moat_grid()		
+		x.grid.count()
+		self.assertEqual(x.grid[5][1].count, 1)
+
+	def test_count_4(self):
+		x = Life(5, 3, [['.','*','.'], ['.','.','.'], ['*','.','*'], ['*','.','*'], ['.','.','*']])
+		x.moat_grid()
+		x.grid.count()
+		self.assertEqual(x.grid[4][1].count, 1)
+
+	def test_count_5(self):
+		x = Life(5, 3, [['.','*','.'], ['.','.','.'], ['*','.','*'], ['*','.','*'], ['.','.','*']])
+		x.moat_grid()
+		x.grid.count()
+		self.assertEqual(x.grid[1][2].count, 0)
+
+	def test_count_6(self):
+		x = Life(5, 3, [['-','-','-'], ['-','-','-'], ['0','-','-'], ['-','-','0'], ['*','0','-']])
+		x.moat_grid()
+		x.grid.count()
+		self.assertEqual(x.grid[5][2].count, 1)
+	
 	# future
 	def test_future_1(self):
-		x = Life(5, 3, [['-','-','-'], ['0','1','0'], ['0','-','-'], ['-','-','0'], ['*','0','-']])
+		x = Life(5, 3, [['-','0','-'], ['-','-','-'], ['-','-','-'], ['-','-','-'], ['-','-','-']])
 		x.moat_grid()
 		x.count()
 		x.future()
@@ -199,14 +336,28 @@ class TestLife(TestCase):
 		x.moat_grid()
 		x.count()
 		x.future()
-		self.assertEqual(x.grid[2][2].state, '*')
+		self.assertEqual(x.grid[3][3].state, '*')
 	
 	def test_future_3(self):
-		x = Life(5, 3, [['.','*','.'], ['.','.','.'], ['*','.','*'], ['*','.','*'], ['.','.','*']])
+		x = Life(5, 3, [['.','*','.'], ['.','.','.'], ['*','.','*'], ['*','*','*'], ['.','.','*']])
 		x.moat_grid()
 		x.count()
 		x.future()
-		self.assertEqual(x.grid[1][2].state, '.')
+		self.assertEqual(x.grid[2][3].state, '.')
+
+	def test_future_4(self):
+		x = Life(5, 3, [['-','0','-'], ['-','-','-'], ['-','-','-'], ['-','-','-'], ['-','-','-']])
+		x.moat_grid()
+		x.count()
+		x.future()
+		self.assertEqual(x.grid[2][2].state, '-')
+
+	def test_future_5(self):
+		x = Life(5, 3, [['.','*','.'], ['.','.','.'], ['*','.','*'], ['*','*','.'], ['.','.','*']])
+		x.moat_grid()
+		x.count()
+		x.future()
+		self.assertEqual(x.grid[4][3].state, '*')
 
 	# life read
 	def test_life_read_1(self):
